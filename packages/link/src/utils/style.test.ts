a17ba96733ef28fbe0b47c5634cc3b7f32404f1f
Vitalii Movchan <vitaliy.movchan@getfront.com>
2023-10-27 17:55:19 +0300

    test check

--- packages/link/src/utils/style.test.ts
+++ packages/link/src/utils/style.test.ts
@@ -6,22 +6,26 @@ describe('Test style utils', () => {
     const received = getLinkStyle(link)
     expect(received).toEqual({ ir: 24, io: 0.1 })
   })
-  test('verify function return nothing on missing query param', () => {
+
+  test('verify function returns nothing on missing query param', () => {
     const link = 'https://some.domain&other=12'
     const received = getLinkStyle(link)
     expect(received).toBeNull()
   })
-  test('verify function return nothing on wrong encoded value', () => {
+
+  test('verify function returns nothing on wrong encoded value', () => {
     const link = 'https://some.domain&link_style=eyJpciI6IDI0LCAiaW8iOiAwL'
     const received = getLinkStyle(link)
     expect(received).toBeUndefined()
   })
-  test('verify function return nothing on empty link', () => {
+
+  test('verify function returns nothing on empty link', () => {
     const link = ''
     const received = getLinkStyle(link)
     expect(received).toBeNull()
   })
-  test('verify function return correct number', () => {
+
+  test('verify function returns correct number', () => {
     expect(getNumber(10, 11)).toBe(11)
     expect(getNumber(10, undefined)).toBe(10)
   })
